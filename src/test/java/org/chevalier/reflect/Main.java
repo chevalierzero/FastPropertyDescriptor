4e9668c7e0a7b3fe264ddcd944e4737cc8444992
chevalier_zero@hotmail.com <chevalier_zero@hotmail.com>
2016-05-16 14:10:39 +0800

    modified:   Main.java

--- src/test/java/org/chevalier/reflect/Main.java
+++ src/test/java/org/chevalier/reflect/Main.java
@@ -4,28 +4,28 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
-		AccessMethod as = AccessMethodFactory.getAccessMethod(TestEntity.class);
+		AccessMethod accessMethod = AccessMethodFactory.getAccessMethod(TestEntity.class);
 		TestEntity testEntity = TestEntity.class.newInstance();
 		
-		as.set(testEntity, "intValue", Integer.MAX_VALUE);
-		as.set(testEntity, "shortValue", Short.MAX_VALUE);
-		as.set(testEntity, "longValue", Long.MAX_VALUE);
-		as.set(testEntity, "floatValue", Float.MAX_VALUE);
-		as.set(testEntity, "doubleValue", Double.MAX_VALUE);
-		as.set(testEntity, "charValue", 'A');
-		as.set(testEntity, "byteValue", Byte.MAX_VALUE);
-		as.set(testEntity, "booleanValue", true);
-		as.set(testEntity, "stringValue", "Hello World!");
+		accessMethod.set(testEntity, "intValue", Integer.MAX_VALUE);
+		accessMethod.set(testEntity, "shortValue", Short.MAX_VALUE);
+		accessMethod.set(testEntity, "longValue", Long.MAX_VALUE);
+		accessMethod.set(testEntity, "floatValue", Float.MAX_VALUE);
+		accessMethod.set(testEntity, "doubleValue", Double.MAX_VALUE);
+		accessMethod.set(testEntity, "charValue", 'A');
+		accessMethod.set(testEntity, "byteValue", Byte.MAX_VALUE);
+		accessMethod.set(testEntity, "booleanValue", true);
+		accessMethod.set(testEntity, "stringValue", "Hello World!");
 
-		System.out.println(as.get(testEntity, "intValue"));
-		System.out.println(as.get(testEntity, "shortValue"));
-		System.out.println(as.get(testEntity, "longValue"));
-		System.out.println(as.get(testEntity, "floatValue"));
-		System.out.println(as.get(testEntity, "doubleValue"));
-		System.out.println(as.get(testEntity, "charValue"));
-		System.out.println(as.get(testEntity, "byteValue"));
-		System.out.println(as.get(testEntity, "booleanValue"));
-		System.out.println(as.get(testEntity, "stringValue"));
+		System.out.println(accessMethod.get(testEntity, "intValue"));
+		System.out.println(accessMethod.get(testEntity, "shortValue"));
+		System.out.println(accessMethod.get(testEntity, "longValue"));
+		System.out.println(accessMethod.get(testEntity, "floatValue"));
+		System.out.println(accessMethod.get(testEntity, "doubleValue"));
+		System.out.println(accessMethod.get(testEntity, "charValue"));
+		System.out.println(accessMethod.get(testEntity, "byteValue"));
+		System.out.println(accessMethod.get(testEntity, "booleanValue"));
+		System.out.println(accessMethod.get(testEntity, "stringValue"));
 	}
 
 }
