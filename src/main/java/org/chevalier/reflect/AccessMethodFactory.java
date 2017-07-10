00b77f55cf6182ca8933ca519d55825e2aab5f1a
chevalier <chevalier_zero@hotmail.com>
2017-07-10 15:14:56 +0800

    update

--- src/main/java/org/chevalier/reflect/AccessMethodFactory.java
+++ src/main/java/org/chevalier/reflect/AccessMethodFactory.java
@@ -10,7 +10,7 @@ public final class AccessMethodFactory {
 	
 	private final static SimpleMap<String, AccessMethod> ACCESS_METHODS = new SimpleMap<String, AccessMethod>();
 	
-	public static AccessMethod getAccessMethod(Class<?> clz) {
+	public static AccessMethod build(Class<?> clz) {
 		
 		if(clz == null){
 			
