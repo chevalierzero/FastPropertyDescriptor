b434fed2261dc42c874e975b18a4566b1906292e
chevalier <chevalier_zero@hotmail.com>
2017-07-10 16:12:13 +0800

    update

--- src/test/java/org/chevalier/reflect/Main.java
+++ src/test/java/org/chevalier/reflect/Main.java
@@ -26,17 +26,17 @@ public class Main {
 		accessMethod.set(testEntity, "objectValues", new Object[]{new Object(), new Object()});
 
 		System.out.println("byteValue : " + accessMethod.get(testEntity, "byteValue", Byte.class));
-		System.out.println("charValue : " + accessMethod.get(testEntity, "charValue"));
-		System.out.println("shortValue : " + accessMethod.get(testEntity, "shortValue"));
+		System.out.println("charValue : " + accessMethod.get(testEntity, "charValue", Character.class));
+		System.out.println("shortValue : " + accessMethod.get(testEntity, "shortValue", Short.class));
 		System.out.println("intValue : " + accessMethod.get(testEntity, "intValue", Integer.class));
-		System.out.println("longValue : " + accessMethod.get(testEntity, "longValue"));
-		System.out.println("floatValue : " + accessMethod.get(testEntity, "floatValue"));
-		System.out.println("doubleValue : " + accessMethod.get(testEntity, "doubleValue"));
-		System.out.println("booleanValue : " + accessMethod.get(testEntity, "booleanValue"));
-		System.out.println("stringValue : " + accessMethod.get(testEntity, "stringValue"));
-		System.out.println("enumValue : " + accessMethod.get(testEntity, "enumValue"));
-		System.out.println("objectValue : " + accessMethod.get(testEntity, "objectValue"));
-		System.out.println("intValues : " + Arrays.toString((int[])accessMethod.get(testEntity, "intValues")));
-		System.out.println("objectValues : " + Arrays.toString((Object[])accessMethod.get(testEntity, "objectValues")));
+		System.out.println("longValue : " + accessMethod.get(testEntity, "longValue", Long.class));
+		System.out.println("floatValue : " + accessMethod.get(testEntity, "floatValue", Float.class));
+		System.out.println("doubleValue : " + accessMethod.get(testEntity, "doubleValue", Double.class));
+		System.out.println("booleanValue : " + accessMethod.get(testEntity, "booleanValue", Boolean.class));
+		System.out.println("stringValue : " + accessMethod.get(testEntity, "stringValue", String.class));
+		System.out.println("enumValue : " + accessMethod.get(testEntity, "enumValue", Enum.class));
+		System.out.println("objectValue : " + accessMethod.get(testEntity, "objectValue", Object.class));
+		System.out.println("intValues : " + Arrays.toString(accessMethod.get(testEntity, "intValues", int[].class)));
+		System.out.println("objectValues : " + Arrays.toString((Object[])accessMethod.get(testEntity, "objectValues", Object[].class)));
 	}
 }
